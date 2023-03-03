5c2adccb946482498556366c1b05c33cc0221059
Duy Lu Hoang <duy.lh@itss.edu.vn>
2023-03-03 17:00:23 +0700

    connect firebase

---
+++ client/src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'wedoc-f1743',
+    appId: '1:1092312971962:web:d61ff5fa47dda7144fe083',
+    storageBucket: 'wedoc-f1743.appspot.com',
+    apiKey: 'AIzaSyC1CDFlOfv81eXiB-j9LTF3Hy3oX7x6Ku4',
+    authDomain: 'wedoc-f1743.firebaseapp.com',
+    messagingSenderId: '1092312971962',
+  },};
